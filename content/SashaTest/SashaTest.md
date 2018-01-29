741325a7de802bc41102d5400b26e1cb365b3f00
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-01-29 08:21:41 +0300

    feat(plugin): Add photos plugin
    Add photos plugin for bugreport demonstration

--- content/SashaTest/SashaTest.md
+++ content/SashaTest/SashaTest.md
@@ -1,39 +1,14 @@
 Title: ValeriyaSpeller
-Date: 2017-04-04 04:14:07
-Modified: 2017-04-04 04:14:07
-Version: 0.1.0
+Date: 2017-07-13 23:51:25
+Modified: 2018-01-29 06:54:20
 Author: Sasha Chernykh
 Lang: en
 Summary: Search typos in Russian texts
-Pagetitle: ValeriyaSpeller
-Metacontent: Implementation yaspeller for Sublime Text
-Pagecolors: sublime-text
-Iconleftorright: right
 Tags: sublime-text, yaspeller
 Category: Sublime Text
-Noco: 1g9b6
-Jquery: true
-Mailhu: true
-Fancybox: true
-Rainbow: true
-Tooltipster: true
-Clipboardjs: true
-Detailspolyfill: false
+image: {photo}kris/Hairs.jpg
 
-Search in [Кошка](kristinita>кошка), ([here](wikipedia_es>python) in Spanish). You can also [search](ddg>python) it.
+Here are my best photos, taken with my favorite camera:
 
-SashaGoddess
-KristinaGoddess
-
-!!! note
-    You should note that the title will be automatically capitalized.
-
-This is a paragraph.
-{: #an_id .a_class }
-
-Apple
-:   Pomaceous fruit of plants of the genus Malus in the family Rosaceae.
-    ![Sublime Text Window](http://i.imgur.com/bqRl8eD.png)
-
-Orange
-:   The fruit of an evergreen tree of the genus Citrus.
+![Hairs]({photo}kris/Hairs.jpg)
+![Cat]({photo}kris/Cat.jpg)
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,5 +1,14 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
+# -*- coding: utf-8 -*-
+# @Author: Kristinita
+# @Date: 2017-01-17 17:43:09
+# @Last Modified time: 2018-01-29 06:43:11
+
+import logging
+""" logging pelican module.
+
+For disabling warnings:
+http://docs.getpelican.com/en/stable/settings.html#logging
+"""
 
 import sys
 """sys module.
@@ -9,6 +18,12 @@ For download plugins from “plugins” folder.
 
 # That Pelican see plugins from this folder
 sys.path.append('plugins')
+
+# Do not touch in generation process
+
+OUTPUT_RETENTION = [".git"]
+
+GIT_SHA_METADATA = True
 
 # ****************************************************************************
 # *                                  General                                 *
@@ -21,6 +36,27 @@ SITEURL = 'http://kristinita.ru'
 TIMEZONE = 'Europe/Moscow'
 DEFAULT_LANG = 'en'
 
+# ****************************************************************************
+# *                                   Feed                                   *
+# ****************************************************************************
+
+# Feed generation, perhaps, not desired when developing
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can edit those links in your config file', '#'),)
+
+# Social widget
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
+
 
 # ****************************************************************************
 # *                                 Markdown                                 *
@@ -29,16 +65,68 @@ DEFAULT_LANG = 'en'
 # Include Markdown extensions
 # http://docs.getpelican.com/en/stable/settings.html?highlight=MARKDOWN#basic-settings
 # https://pythonhosted.org/Markdown/extensions/#third-party-extensions
+
+"""python markdown extensions.
+
+Extensions, which Sasha's fan use.
+
+#
+# 1. extra
+#
+
+extra — set of extensions
+
+Attribute List — add id and classes in Markdown
+https://pythonhosted.org/Markdown/extensions/attr_list.html
+
+Definition List — add dl, dd, dt tags
+https://pythonhosted.org/Markdown/extensions/definition_lists.html
+https://webref.ru/html/dd
+
+Fenced code blocks — correct display multiline code blocks
+https://pythonhosted.org/Markdown/extensions/fenced_code_blocks.html
+
+Tables — simple tables
+https://pythonhosted.org/Markdown/extensions/tables.html
+
+#
+# 2. Other default extensions
+#
+
+admonition — notes in Markdown
+https://pythonhosted.org/Markdown/extensions/admonition.html
+
+nl2br — new line without 2 br in line end
+Like GitHub, not like Stack Overflow
+https://pythonhosted.org/Markdown/extensions/nl2br.html
+
+sane_lists — mix ordered/unordered list
+https://pythonhosted.org/Markdown/extensions/sane_lists.html
+
+#
+# 3. facelessuser extensions
+#
+
+inlinehilite — inline highlight code
+http://facelessuser.github.io/pymdown-extensions/extensions/inlinehilite/
+https://pythonhosted.org/Markdown/extensions/code_hilite.html#step-2-add-css-classes
+
+betterem — fix ** and __ symbols
+http://facelessuser.github.io/pymdown-extensions/extensions/betterem/
+
+"""
+
 MARKDOWN = {
     'extension_configs': {
-        # Fenced code blocks
-        # Correct displaying code blocks
-        # https://pythonhosted.org/Markdown/extensions/fenced_code_blocks.html
-        'markdown.extensions.extra': {},
-        # Admonition
-        # Notes rendering
-        # https://pythonhosted.org/Markdown/extensions/admonition.html
-        'markdown.extensions.admonition': {},
+        # 'markdown.extensions.extra': {},
+        # 'markdown.extensions.admonition': {},
+        # 'markdown.extensions.nl2br': {},
+        # 'markdown.extensions.sane_lists': {},
+        # 'pymdownx.inlinehilite': {'css_class': 'SashaInlineHighlight',
+        #                           'style_plain_text': True,
+        #                           'use_codehilite_settings': False, },
+        # 'pymdownx.betterem': {},
+        # 'pymdownx.caret': {},
     },
     'output_format': 'html5',
 }
@@ -48,7 +136,7 @@ MARKDOWN = {
 # ****************************************************************************
 
 # Delete old site before building
-DELETE_OUTPUT_DIRECTORY = True
+DELETE_OUTPUT_DIRECTORY = False
 
 # Do not touch in generation process
 OUTPUT_RETENTION = [".git"]
@@ -68,11 +156,65 @@ OUTPUT_SOURCES = False
 
 PLUGIN_PATHS = ['pelican-plugins']
 PLUGINS = [
-    'pagefixer',
-    'pelican_javascript',
-    'interlinks',
-    'section_number',
+    # 'filetime_from_git',
+    # 'pagefixer',
+    # 'pelican_javascript',
+    # 'interlinks',
+    # 'section_number',
+    'photos'
 ]
+
+PHOTO_ARTICLE = (760, 506, 80)
+
+PHOTO_GALLERY = (1024, 768, 80)
+
+PHOTO_LIBRARY = "pictures"
+
+PHOTO_RESIZE_JOBS = 5
+
+PHOTO_THUMB = (192, 144, 60)
+
+# Deadlinks
+# https://github.com/silentlamb/pelican-deadlinks/tree/2689d7ebc4a3e05b74d681b46d9f6db7fd5f135e
+
+DEADLINK_VALIDATION = True
+
+DEADLINK_OPTS = {
+    # Replace dead link to Internet Archive link
+    'archive': True,
+    'classes': ['SashaDeadlink'],
+    # Disable «403» or «404» text after link in generated HTML
+    'labels': False
+}
+
+# Sitemap
+# https://github.com/getpelican/pelican-plugins/tree/master/sitemap
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0,
+        'indexes': 0.5,
+        'pages': 1
+    },
+    'changefreqs': {
+        'articles': 'never',
+        'indexes': 'weekly',
+        'pages': 'always'
+    }
+}
+
+# Section number
+# https://github.com/getpelican/pelican-plugins/tree/master/section_number
+
+SECTION_NUMBER_MAX = 5
+
+# Interlinks for generate links for frequently used sites
+# https://github.com/getpelican/pelican-plugins/tree/master/interlinks
+INTERLINKS = {
+    'kristinita': 'http://kristinita.ru/#gsc.tab=0&gsc.q='
+}
+
 
 # ****************************************************************************
 # *                                   Paths                                  *
@@ -86,14 +228,13 @@ PATH = 'content'
 # Don't set “PAGE_PATHS = ['']”! See
 # https://github.com/getpelican/pelican/issues/2123
 
-PAGE_PATHS = ['Gingerinas', 'Giologica', 'Life-hacks', 'Sasha-Black',
-              'Smert-svobode', 'Sublime-Text']
+PAGE_PATHS = ['None']
 
 # Path for articles
 # [Hack] That exclude articles, include non-exicting folder.
 # «ARTICLE_PATHS = None» — critical error, integer parameter
 
-ARTICLE_PATHS = ['None']
+ARTICLE_PATHS = ['SashaTest']
 
 # Non-modified files and folders
 # [Hack] The register matters. 404.md & 404.html don't work.
@@ -126,3 +267,15 @@ THEME = 'themes/sashapelican'
 # Pagination — division of articles
 
 DEFAULT_PAGINATION = False
+
+# Typogrify library
+# https://pypi.python.org/pypi/typogrify
+# False, because bug with br in a newline:
+# https://github.com/mintchaos/typogrify/pull/39
+# Also unexpected &nbsp; after last word in line.
+
+TYPOGRIFY = False
+
+# Disable logging of empty alt attribute, needs “import logging” module
+
+LOG_FILTER = [(logging.WARN, 'Empty alt attribute for image %s in %s')]
