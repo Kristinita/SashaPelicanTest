9f4a1b31933fcfa9dde6c66d0bee84c608b5f2a4
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-02-28 19:59:20 +0300

    ci(appveyor): Correct bat file path

---
+++ appveyor.bat
@@ -0,0 +1,6 @@
+@REM @Author: Kristinita
+@REM @Date:   2018-02-28 19:42:16
+@REM @Last Modified by:   Kristinita
+@REM Modified time: 2018-02-28 19:42:47
+start cmd /C "pip install pipenv & pipenv install --dev"
+start cmd /C "npm install -g grunt-cli & npm install"
