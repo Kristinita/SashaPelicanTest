5caf70786a33997753aea19bd7f7e7d5a0ca5daa
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-03-01 11:56:13 +0300

    ci(travis): Add logging

--- travis.sh
+++ travis.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-(pip install pipenv && pipenv install --dev) &
-(npm install -g grunt-cli && npm install) &
+(pip install pipenv && pipenv install --dev) >& pip.log &
+(npm install -g grunt-cli && npm install) >& npm.log &
