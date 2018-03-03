9aec0994e30b990905cbea5c1f835cf31c708d62
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-03-03 18:39:40 +0300

    ci(Travis): Remove ".." from paths

--- grunt/tidy.sh
+++ grunt/tidy.sh
@@ -1,14 +1,14 @@
 # @Author: Kristinita
 # @Date:   2018-03-03 13:47:42
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-03-03 18:33:19
+# @Last Modified time: 2018-03-03 18:39:04
 # Recursive glob:
 # https://unix.stackexchange.com/a/49917/237999
 shopt -s globstar
-cd ../output
+cd output
 # Using variables in script:
 # http://tldp.org/HOWTO/Bash-Prog-Intro-HOWTO-7.html
 for i in **/*.html; do
 	echo $i
-	tidy -mq -config "../tidy-config.txt" $i
+	tidy -mq -config "tidy-config.txt" $i
 done
