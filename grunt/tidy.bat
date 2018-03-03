2abb12b1751958360a628318cfc38fc16fcf58a0
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-03-03 19:26:58 +0300

    ci(travis): Use command instead of shell

--- grunt/tidy.bat
+++ grunt/tidy.bat
@@ -1,6 +1,9 @@
 @REM @Author: Kristinita
 @REM @Date:   2018-03-03 07:55:41
 @REM @Last Modified by:   Kristinita
-@REM Modified time: 2018-03-03 18:48:37
+@REM Modified time: 2018-03-03 19:21:28
+rem Disable echo command and cwd in output:
+rem https://serverfault.com/a/187358/351829
+@echo off
 cd output
 FOR /R %%i IN (*.html) DO echo %%i & tidy -mq --wrap 0 %%i
--- grunt/tidy.sh
+++ grunt/tidy.sh
@@ -1,7 +1,7 @@
 # @Author: Kristinita
 # @Date:   2018-03-03 13:47:42
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-03-03 18:56:46
+# @Last Modified time: 2018-03-03 19:25:40
 # Recursive glob:
 # https://unix.stackexchange.com/a/49917/237999
 shopt -s globstar
@@ -9,6 +9,6 @@ cd output
 # Using variables in script:
 # http://tldp.org/HOWTO/Bash-Prog-Intro-HOWTO-7.html
 for i in **/*.html; do
-	echo $i &&
+	echo $i
 	tidy -mq --wrap 0 $i
 done
