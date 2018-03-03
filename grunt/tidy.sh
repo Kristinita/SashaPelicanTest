1f0d5cf466580575b31549225a1f288d973f87e7
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-03-03 19:39:32 +0300

    ci(travis): Revert echo and script

--- grunt/tidy.sh
+++ grunt/tidy.sh
@@ -1,13 +1,13 @@
 # @Author: Kristinita
 # @Date:   2018-03-03 13:47:42
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-03-03 19:31:52
+# @Last Modified time: 2018-03-03 19:38:48
 # Recursive glob:
 # https://unix.stackexchange.com/a/49917/237999
 shopt -s globstar
 # Using variables in script:
 # http://tldp.org/HOWTO/Bash-Prog-Intro-HOWTO-7.html
 for i in **/*.html; do
-	echo $i
 	tidy -mq --wrap 0 $i
+	echo $i
 done
