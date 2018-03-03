05efdc6f72244cbd0d6c3fd16114a5398e1d7ba9
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-03-03 18:52:43 +0300

    ci(travis): wrap 0 for Travis CI, not for AppVeyor

--- grunt/tidy.sh
+++ grunt/tidy.sh
@@ -1,7 +1,7 @@
 # @Author: Kristinita
 # @Date:   2018-03-03 13:47:42
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-03-03 18:39:04
+# @Last Modified time: 2018-03-03 18:52:13
 # Recursive glob:
 # https://unix.stackexchange.com/a/49917/237999
 shopt -s globstar
@@ -10,5 +10,5 @@ cd output
 # http://tldp.org/HOWTO/Bash-Prog-Intro-HOWTO-7.html
 for i in **/*.html; do
 	echo $i
-	tidy -mq -config "tidy-config.txt" $i
+	tidy -mq --wrap 0 $i
 done
