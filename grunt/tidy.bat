d5d3f553e29a778500ae88ae47804f6f0195ff64
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-03-03 12:56:43 +0300

    ci(travis): Install latest .deb file on Ubuntu
    https://askubuntu.com/a/823272/582218

--- grunt/tidy.bat
+++ grunt/tidy.bat
@@ -1,5 +1,5 @@
 @REM @Author: Kristinita
 @REM @Date:   2018-03-03 07:55:41
 @REM @Last Modified by:   Kristinita
-@REM Modified time: 2018-03-03 12:37:17
-FOR /R %%i IN (*.html) DO tidy -mq -utf8 -config "../tidy-config.txt" %%i
+@REM Modified time: 2018-03-03 12:54:05
+FOR /R %%i IN (*.html) DO tidy -mq -config "../tidy-config.txt" %%i
