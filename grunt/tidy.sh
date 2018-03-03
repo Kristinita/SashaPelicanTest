5a676c7a0f7a0c5c293bb2d6449969e58790e3ae
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-03-03 20:01:50 +0300

    ci(travis): printf instead of echo

--- grunt/tidy.sh
+++ grunt/tidy.sh
@@ -1,13 +1,13 @@
 # @Author: Kristinita
 # @Date:   2018-03-03 13:47:42
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-03-03 19:38:48
+# @Last Modified time: 2018-03-03 20:01:01
 # Recursive glob:
 # https://unix.stackexchange.com/a/49917/237999
 shopt -s globstar
 # Using variables in script:
 # http://tldp.org/HOWTO/Bash-Prog-Intro-HOWTO-7.html
 for i in **/*.html; do
+	printf "$i\n"
 	tidy -mq --wrap 0 $i
-	echo $i
 done
