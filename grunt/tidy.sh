1b8b0e8d3622a665b90d57bfe8d60a9c1eeb0834
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-03-03 19:34:09 +0300

    ci(Travis): Move cd from script

--- grunt/tidy.sh
+++ grunt/tidy.sh
@@ -1,11 +1,10 @@
 # @Author: Kristinita
 # @Date:   2018-03-03 13:47:42
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-03-03 19:25:40
+# @Last Modified time: 2018-03-03 19:31:52
 # Recursive glob:
 # https://unix.stackexchange.com/a/49917/237999
 shopt -s globstar
-cd output
 # Using variables in script:
 # http://tldp.org/HOWTO/Bash-Prog-Intro-HOWTO-7.html
 for i in **/*.html; do
