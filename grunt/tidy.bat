8c365c548b428fbd30f3aaaf24aab40d1652ac8e
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-03-03 18:44:58 +0300

    ci(travis): Try ".." for Travis path

--- grunt/tidy.bat
+++ grunt/tidy.bat
@@ -1,6 +1,6 @@
 @REM @Author: Kristinita
 @REM @Date:   2018-03-03 07:55:41
 @REM @Last Modified by:   Kristinita
-@REM Modified time: 2018-03-03 18:38:00
+@REM Modified time: 2018-03-03 18:44:26
 cd output
-FOR /R %%i IN (*.html) DO echo %%i & tidy -mq -config "tidy-config.txt" %%i
+FOR /R %%i IN (*.html) DO echo %%i & tidy -mq -config "../tidy-config.txt" %%i
