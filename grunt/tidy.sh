820d1d7cc43b7b78a4fd75ee4014db6a9398ca5c
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-03-03 16:03:20 +0300

    ci(travis): Platform-specific grunt-shell settings

--- grunt/tidy.sh
+++ grunt/tidy.sh
@@ -1,13 +1,13 @@
 # @Author: Kristinita
 # @Date:   2018-03-03 13:47:42
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-03-03 15:27:06
+# @Last Modified time: 2018-03-03 15:48:10
 # Recursive glob:
 # https://unix.stackexchange.com/a/49917/237999
 shopt -s globstar
 # Using variables in script:
 # http://tldp.org/HOWTO/Bash-Prog-Intro-HOWTO-7.html
 for i in **/*.html; do
-	tidy -mq -config "../tidy-config.txt" $i
 	echo $i
+	tidy -mq -config "../tidy-config.txt" $i
 done
