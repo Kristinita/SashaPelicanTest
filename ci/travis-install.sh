b0b116c00e7a08ff6fc508b1f3683ce01f068ab7
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-03-07 20:01:20 +0300

    ci(travis): Add sequential Tidy installation

--- ci/travis-install.sh
+++ ci/travis-install.sh
@@ -5,5 +5,4 @@
 # [NOTE] Grunt tasks needs grunt-cli globally:
 # https://stackoverflow.com/a/21133609/5951529
 parallel ::: 'pip install --upgrade pip && pip install pipenv && pipenv install --dev' \
-             'npm install --global npm && npm install -g grunt-cli && npm install' \
-             'bash ../tidy/tidy-install.sh'
+             'npm install --global npm && npm install -g grunt-cli && npm install'
