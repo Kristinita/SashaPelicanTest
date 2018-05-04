bd8cd8918db612166bdd12423c39751806346d09
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-05-04 10:35:22 +0300

    ci(appveyor): try revert pelicanconf.py

--- batch/appveyor-install.bat
+++ batch/appveyor-install.bat
@@ -1,7 +1,7 @@
 @REM @Author: Kristinita
 @REM @Date:   2018-02-28 19:42:16
 @REM @Last Modified by:   Kristinita
-@REM Modified time: 2018-04-28 11:43:10
+@REM Modified time: 2018-05-04 10:34:50
 @REM Install all AppVeyor dependencies for building and validation.
 @REM Use REM comments:
 @REM https://stackoverflow.com/a/12407934/5951529
@@ -15,15 +15,11 @@
 @REM [WARNING] Use -ignoredependencies for HTML Tidy installation to AppVeyor
 @REM https://github.com/majkinetor/au-packages/issues/75
 @REM https://ci.appveyor.com/project/Kristinita/sashatidydebugging/build/1.0.12
-START /B CMD /C "choco install html-tidy -y -ignoredependencies"
 @REM [NOTE] Needs “python -m pip install -U pip”, not “pip install -U pip”, because
 @REM “PermissionError: [WinError 5] Access is denied”:
 @REM https://pip.pypa.io/en/stable/installing/#upgrading-pip
 @REM https://pythonhosted.org/CodeChat/appveyor.yml.html
 START /B CMD /C "python -m pip install --upgrade pip & pip install pipenv & pipenv install --dev"
-START /B CMD /C "choco install nodejs -y & npm install -g grunt-cli & npm install"
-START /B CMD /C "gem install travis"
 @REM [NOTE] ShellCheck installation via Scoop — fast way for Windows:
 @REM https://github.com/koalaman/shellcheck/issues/915#issuecomment-385049702
 @REM https://help.appveyor.com/discussions/suggestions/843-add-support-for-the-scoop-installer
-START /B CMD /C "scoop install shellcheck"
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # @Author: Kristinita
 # @Date: 2017-01-17 17:43:09
-# @Last Modified time: 2018-05-03 18:01:26
+# @Last Modified time: 2018-05-04 10:32:55
 """Pelican configuration file.
 
-For development. publishconf.py - for publishing.
+For development. publishconf.py — for publishing.
 """
 
 # Parse YAML configuration file:
@@ -12,6 +12,13 @@ For development. publishconf.py - for publishing.
 # Require PyYAML module:
 # http://pyyaml.org/wiki/PyYAML
 import yaml
+YAMLCONFIG = yaml.load(open('pelicanvariables.yaml'))
+PATH = YAMLCONFIG['path']
+OUTPUT_PATH = YAMLCONFIG['output_path']
+THEME = YAMLCONFIG['theme']
+THEME_STATIC_DIR = YAMLCONFIG['theme_static_dir']
+THEME_STATIC_PATHS = YAMLCONFIG['theme_static_paths']
+
 
 # [DEPRECATED] All warnings fixed:
 # For disabling warnings:
@@ -22,35 +29,12 @@ import yaml
 # *                                  General                                 *
 # ****************************************************************************
 
-# Relative paths for development.
+AUTHOR = 'Саша Черных'
+SITENAME = 'Поиск Кристиниты'
 SITEURL = '.'
 
-# Configuration from YAML files.
-YAMLCONFIG = yaml.load(open('pelicanvariables.yaml'))
-
-PATH = YAMLCONFIG['path']
-OUTPUT_PATH = YAMLCONFIG['output_path']
-THEME = YAMLCONFIG['theme']
-THEME_STATIC_DIR = YAMLCONFIG['theme_static_dir']
-THEME_STATIC_PATHS = YAMLCONFIG['theme_static_paths']
-PLUGIN_PATHS = YAMLCONFIG['plugin_paths']
-
-AUTHOR = YAMLCONFIG['author']
-SITENAME = YAMLCONFIG['sitename']
-TIMEZONE = YAMLCONFIG['timezone']
-DEFAULT_LANG = YAMLCONFIG['default_lang']
-CACHE_PATH = YAMLCONFIG['cache_path']
-
-# Path for pages
-# [Hack] That exclude articles, include non-exicting folder.
-# «ARTICLE_PATHS = None» - critical error, integer parameter
-PAGE_PATHS = YAMLCONFIG['page_paths']
-# Path for articles
-# Don't set "PAGE_PATHS = ['']"! See
-# https://github.com/getpelican/pelican/issues/2123
-ARTICLE_PATHS = YAMLCONFIG['article_paths']
-
-GITHUB_URL = YAMLCONFIG['github_url']
+TIMEZONE = 'Europe/Moscow'
+DEFAULT_LANG = 'en'
 
 # ****************************************************************************
 # *                                Generation                                *
@@ -60,16 +44,19 @@ GITHUB_URL = YAMLCONFIG['github_url']
 # I don't get bugs, if true.
 # If false, files, which I delete from content directory, not deleted from
 # output directory.
+
 DELETE_OUTPUT_DIRECTORY = False
 
 # Do not touch in generation process
-OUTPUT_RETENTION = ['.git']
+
+OUTPUT_RETENTION = [".git"]
 
 # Cache settings
 # http://docs.getpelican.com/en/latest/settings.html?highlight=cache
 # [NOTE] If experiments with Pelican settings, use --ignore-cache option
 # http://docs.getpelican.com/en/latest/settings.html#settings
 CACHE_CONTENT = True
+CACHE_PATH = 'pelican_cache'
 GZIP_CACHE = True
 # [NOTE] WITH_FUTURE_DATES conflict with CONTENT_CACHING_LAYER
 # http://docs.getpelican.com/en/latest/settings.html#reading-only-modified-content
@@ -77,11 +64,17 @@ CONTENT_CACHING_LAYER = 'reader'
 LOAD_CONTENT_CACHE = True
 
 # Relative URLs for developing
+
 RELATIVE_URLS = True
 
 # Original format without rendering. If true, md rendering to md,
 # if false, md → html
+
 OUTPUT_SOURCES = False
+
+# GitHub path for this project
+
+GITHUB_URL = 'https://github.com/Kristinita/KristinitaPelican'
 
 # ****************************************************************************
 # *                                  Plugins                                 *
@@ -91,17 +84,18 @@ OUTPUT_SOURCES = False
 
 https://github.com/getpelican/pelican-plugins
 
-filetime_from_git - add "date" and "modified" varables from Git:
-If plugin exist: you don't need add "Date:" and "Modified:" variables for
+filetime_from_git — add “date” and “modified” varables from Git:
+If plugin exist: you don't need add “Date:” and “Modified:” variables for
 your each article:
 https://github.com/getpelican/pelican-plugins/tree/master/filetime_from_git
 
-liquid_tags - add videos/images by code;
-for Instagram images, without another frames data;
+liquid_tags — add videos/images by code;
+for Instagram only images, without additional data;
 https://github.com/getpelican/pelican-plugins/tree/master/liquid_tags
 
 """
 
+PLUGIN_PATHS = ['pelican-plugins']
 # [BUG] Automatic plugins installation doesn't work for me:
 # https://github.com/kplaube/pelican-plugin-installer/issues/6
 PLUGINS = [
@@ -115,16 +109,13 @@ PLUGINS = [
     'neighbors',
     # [FIXME] Different colors for different designs
     # 'pelican-linkclass',
-    # [DONE]
-    # # [BUG] "TypeError: Unicode-objects must be encoded before hashing":
-    # # https://github.com/getpelican/pelican-plugins/issues/1011
-    # [DEPRECATED] Anchors not support, but all my internal links with anchors:
-    # https://github.com/getpelican/pelican-plugins/issues/1025
+    # [BUG] “TypeError: Unicode-objects must be encoded before hashing”:
+    # https://github.com/getpelican/pelican-plugins/issues/1011
     # 'permalinks',
     # 'photos',
     # Disable, because:
     # 1. Plugin create duplicates of files
-    # 2. Incorrect paths - http://bit.ly/2pZdyk0
+    # 2. Incorrect paths — http://bit.ly/2pZdyk0
     # 'pelican_javascript',
     'putsashi',
     # [BUG] Incorrect Cyrillic URL's:
@@ -133,12 +124,13 @@ PLUGINS = [
     'section_number',
 ]
 
-# PHOTO_LIBRARY = 'pictures'
+# PHOTO_LIBRARY = "pictures"
 
 # PHOTO_THUMB = (192, 144, 60)
 
 # Deadlinks
 # https://github.com/silentlamb/pelican-deadlinks/
+
 DEADLINK_VALIDATION = True
 
 DEADLINK_OPTS = {
@@ -146,13 +138,13 @@ DEADLINK_OPTS = {
     'archive': True,
     'classes': ['SashaDeadlink'],
     # Disable «403» or «404» text after link in generated HTML
-    'labels': False,
+    'labels': False
 }
 
 # just_table
 # Simple Pelican tables
 # https://github.com/burakkose/just_table
-# [BUG] Unwanted <p> tag - https://github.com/burakkose/just_table/issues/5
+# [BUG] Unwanted <p> tag — https://github.com/burakkose/just_table/issues/5
 JTABLE_SEPARATOR = '|'
 
 # filetime_from_git
@@ -166,6 +158,7 @@ RANDOM = 'random.html'
 
 # Sitemap
 # https://github.com/getpelican/pelican-plugins/tree/master/sitemap
+
 SITEMAP = {
     'format': 'xml',
     'priorities': {
@@ -182,14 +175,15 @@ SITEMAP = {
 
 # Section number
 # https://github.com/getpelican/pelican-plugins/tree/master/section_number
+
 SECTION_NUMBER_MAX = 5
 
 # Interlinks for generate links for frequently used sites
 # https://github.com/getpelican/pelican-plugins/tree/master/interlinks
+
 INTERLINKS = {
     'kristinita': 'https://kristinita.ru/#gsc.tab=0&gsc.q='
 }
-
 
 # ****************************************************************************
 # *                                 Markdown                                 *
@@ -205,82 +199,82 @@ http://romeogolf.github.io/pelican-i-modul-python-markdown.html
 Extensions, which Sasha's fan use.
 
 CLI usage: https://python-markdown.github.io/cli/#using-extensions
-Example: "python -m markdown -x pymdownx.superfences -c config.yml SashaSuperFences.md"
+Example: “python -m markdown -x pymdownx.superfences -c config.yml SashaSuperFences.md”
 
-Officially support extensions doesn't need extra installation:
+Officially support extensions doesn't need additional installation:
 https://python-markdown.github.io/extensions/#officially-supported-extensions
 
 #
 # 1. extra
 #
 
-Attribute List - add id and classes in Markdown
+Attribute List — add id and classes in Markdown
 https://python-markdown.github.io/extensions/extra/
 
-Definition List - add dl, dd, dt tags
+Definition List — add dl, dd, dt tags
 https://python-markdown.github.io/extensions/definition_lists/
 https://webref.ru/html/dd
 
-Fenced code blocks - correct display multiline code blocks
+Fenced code blocks — correct display multiline code blocks
 https://python-markdown.github.io/extensions/fenced_code_blocks/
 
-Tables - simple tables
+Tables — simple tables
 https://python-markdown.github.io/extensions/tables/
 
 #
 # 2. Other default extensions
 #
 
-admonition - notes in Markdown
+admonition — notes in Markdown
 https://python-markdown.github.io/extensions/admonition/
 
-nl2br - new line without 2 br in line end
+nl2br — new line without 2 br in line end
 Like GitHub, not like Stack Overflow
 https://python-markdown.github.io/extensions/nl2br/
 
-sane_lists - mix ordered/unordered list
+sane_lists — mix ordered/unordered list
 https://python-markdown.github.io/extensions/sane_lists/
 
 #
 # 3. facelessuser extensions
 #
 
-betterem - fix ** and __ symbols
+betterem — fix ** and __ symbols
 http://facelessuser.github.io/pymdown-extensions/extensions/betterem/
 
-caret - underline, if ^^ symbols
+caret — underline, if ^^ symbols
 http://facelessuser.github.io/pymdown-extensions/extensions/caret/
 
-critic - Critic Markup support
+critic — Critic Markup support
 http://facelessuser.github.io/pymdown-extensions/extensions/critic/
 http://criticmarkup.com/
 
-escapeall - don't convert spaces, octotorps and another symbols
+escapeall — don't convert spaces, octotorps and another symbols
 http://facelessuser.github.io/pymdown-extensions/extensions/escapeall/
 https://github.com/waylan/Python-Markdown/issues/564#issuecomment-299681538
 
-inlinehilite - inline highlight code
+inlinehilite — inline highlight code
 http://facelessuser.github.io/pymdown-extensions/extensions/inlinehilite/
 https://pythonhosted.org/Markdown/extensions/code_hilite.html#step-2-add-css-classes
 
-magiclink - clickable Bare URL
+magiclink — clickable Bare URL
 http://facelessuser.github.io/pymdown-extensions/extensions/magiclink/
 
-progressbar - candy Progress Bar
+progressbar — candy Progress Bar
 http://facelessuser.github.io/pymdown-extensions/extensions/progressbar/
 
-smartsymbols - replace symbols to correct.
+smartsymbols — replace symbols to correct.
 Example: (c) → ©
 http://facelessuser.github.io/pymdown-extensions/extensions/smartsymbols/
 
-snippets - insert HTML to another HTML
+snippets — insert HTML to another HTML
 http://facelessuser.github.io/pymdown-extensions/extensions/snippets/
 
-superfences - different block highlights.
+superfences — different block highlights.
 http://facelessuser.github.io/pymdown-extensions/extensions/superfences/
 http://pygments.org/docs/lexers/
 
-tilde - ~~delete~~ and sub support
+tilde — ~~delete~~ and sub support
 http://facelessuser.github.io/pymdown-extensions/extensions/tilde/
 
 #
@@ -289,27 +283,27 @@ http://facelessuser.github.io/pymdown-extensions/extensions/tilde/
 
 https://github.com/Python-Markdown/markdown/wiki/Third-Party-Extensions
 
-downheader - down headers to 1 item, that MD002 is good
+downheader — down headers to 1 item, that MD002 is good
 https://github.com/cprieto/mdx_downheader
 
-markdown_blockdiag - diagrams in Markdown
+markdown_blockdiag — diagrams in Markdown
 https://github.com/gisce/markdown-blockdiag
 http://blockdiag.com/en/blockdiag/index.html
 
-markdown_newtab - add target='_blank' for all links
+markdown_newtab — add target="_blank" for all links
 Attribute don't add for local anchors
 https://github.com/Undeterminant/markdown-newtab/blob/master/run_tests.py
 
-md_environ - add own environment variables to text
+md_environ — add own environment variables to text
 https://github.com/cmacmackin/md-environ
 
-mdx_cite - cite tag
+mdx_cite — cite tag
 https://github.com/aleray/mdx_cite
 
-mdx_custom_span_class - custom !!classes!! for span tag
+mdx_custom_span_class — custom !!classes!! for span tag
 https://github.com/exaroth/mdx_custom_span_class
 
-pyembed - embed videos/images from different popular sites
+pyembed — embed videos/images from different popular sites
 https://pyembed.github.io/usage/markdown/
 """
 
@@ -324,10 +318,10 @@ MARKDOWN = {
         #
         'markdown.extensions.admonition': {},
         # [DEPRECATED] just_table conflict, just_table tables display incorrect.
-        # nl2br - extension, without which I can work.
+        # nl2br — extension, without which I can work.
         # 'markdown.extensions.nl2br': {},
         'markdown.extensions.sane_lists': {},
-        # [DEPRECATED] "section_number" disabling bug:
+        # [DEPRECATED] “section_number” disabling bug:
         # https://github.com/waylan/Python-Markdown/issues/560
         # Now I use regex replacing in Gruntfile.coffee and don't need this extension:
         # 'markdown.extensions.toc': {'permalink': True},
@@ -352,16 +346,14 @@ MARKDOWN = {
         'pymdownx.mark': {},
         # That bars change colors in each 10%:
         # https://github.com/facelessuser/pymdown-extensions/pull/208
-        'pymdownx.progressbar': {
-            'progress_increment': 10
-        },
+        'pymdownx.progressbar': {'progress_increment': 10},
         'pymdownx.smartsymbols': {},
         'pymdownx.snippets': {},
         'pymdownx.superfences': {
             'css_class': 'SashaBlockHighlight',
             # Doesn't convert tabs to spaces in code blocks:
             # https://github.com/marionebl/commitlint/issues/316
-            'preserve_tabs': True,
+            'preserve_tabs': True
         },
         'pymdownx.tilde': {},
         #
@@ -382,15 +374,47 @@ MARKDOWN = {
 # *                                   Paths                                  *
 # ****************************************************************************
 
+# Path for site content
+
+# PATH = 'content'
+
+# Where to output the generated files
+
+# OUTPUT_PATH = 'output'
+
+# Path for articles
+# Don't set “PAGE_PATHS = ['']”! See
+# https://github.com/getpelican/pelican/issues/2123
+
+ARTICLE_PATHS = [
+    'Gingerinas',
+    'Giologica',
+    'IT-articles',
+    'Life-hacks',
+    'Programs',
+    'Smert-svobode',
+    'Sasha-Black',
+    'Sublime-Text'
+]
+
+# Path for pages
+# [Hack] That exclude articles, include non-exicting folder.
+# «ARTICLE_PATHS = None» — critical error, integer parameter
+
+PAGE_PATHS = ['Pages']
+
 # Non-modified files and folders
 # [Hack] The register matters. 404.md & 404.html don't work.
+
 STATIC_PATHS = ['']
 
 # That filename = Slug, not necessary to write slug manually for each article
+
 SLUGIFY_SOURCE = 'basename'
 
 # Extra path metadata
 # http://manos.im/blog/static-site-pelican-grunt-travis-github-pages/
+
 ARTICLE_URL = '{slug}.html'
 PAGE_URL = '{slug}.html'
 PAGE_SAVE_AS = PAGE_URL
@@ -406,7 +430,7 @@ TAGS_SAVE_AS = 'tag/alltags.html'
 
 # Disable feed generation in developing process
 # http://docs.getpelican.com/en/latest/faq.html#what-if-i-want-to-disable-feed-generation
-# [BUG] I get in console: "TypeError: not all arguments converted during string formatting"
+# [BUG] I get in console: “TypeError: not all arguments converted during string formatting”
 
 # FEED_ALL_ATOM = None
 # CATEGORY_FEED_ATOM = None
@@ -418,11 +442,14 @@ TAGS_SAVE_AS = 'tag/alltags.html'
 # *                                   Other                                  *
 # ****************************************************************************
 
-# Pagination - division of articles
+# Pagination — division of articles
+
 DEFAULT_PAGINATION = False
 
-# Disable logging of empty alt attribute, needs "import logging" module
-# Fix, but save as comment, that doesn't forgot if I will have a similar problem.
+# Disable logging of empty alt attribute, needs “import logging” module
+# Fix, but save as comment, that doesn't forgot if I will have a similar
+# problem.
+
 # LOG_FILTER = [(logging.WARN, 'Empty alt attribute for image %s in %s')]
 
 # FORMATTED_FIELDS = ['summary']
@@ -436,4 +463,5 @@ DEFAULT_PAGINATION = False
 # False, because bug with br in a newline:
 # https://github.com/mintchaos/typogrify/pull/39
 # Also unexpected &nbsp; after last word in line.
+
 TYPOGRIFY = False
